39a833706c459229f54d7cd77b1cd52c2d57a83e
Joel Gonzales Tipismana <joelengt@users.noreply.github.com>
2018-04-01 12:07:19 -0500

    Update symbol.js

--- symbol.js
+++ symbol.js
@@ -131,6 +131,16 @@ data
 
 
 
+// sample 3
+
+/* ES7 */
+import { map, takeWhile, forEach } from "iterlib";
+
+getPlayers()
+::map(x => x.character())
+::takeWhile(x => x.strength > 100)
+::forEach(x => console.log(x));
+
 
 // Object Observe
 let persona = {

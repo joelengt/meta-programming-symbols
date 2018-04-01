9e15cc2029888c4853e8e0345368a7567bfa4342
Joel Gonzales Tipismana <joelengt@users.noreply.github.com>
2018-04-01 11:41:09 -0500

    Update symbol.js

--- symbol.js
+++ symbol.js
@@ -1,3 +1,14 @@
+// Symbol('nombre')
+const nombre  = Symbol('nombre');
+
+const persona = {
+  [nombre]: 'Pepito',
+  get nombre() { return persona[nombre] }
+};
+
+console.log(persona.nombre); // permite el acceso al nombre de persona
+persona.nombre = 'Jose'; // da error al querer cambiarlo
+
 
 // Symbol.hasInstance: instanceof
 class MiArray {  

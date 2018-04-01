3bb11308dddaba68c73d477c5e94e2802cad508d
Joel Gonzales Tipismana <joelengt@users.noreply.github.com>
2018-04-01 11:36:52 -0500

    Update symbol.js

--- symbol.js
+++ symbol.js
@@ -104,6 +104,30 @@ map.delete('persona'); // borramos 'persona' y el valor asociado
 console.log(persona);
 console.log(map.get('persona'));
 
+// Set
+let persona = { nombre: 'Pepito' };
+
+let set = new Set([1, 'foo', 3.14, persona]);
+
+console.log(set.size);
+
+console.log(set.has(1));
+
+set.add('bar');
+
+set.delete(persona);
+
+set.forEach(v => console.log(v));
+
+for (let values of set) {
+  console.log(values);
+}
+
+let values = set.values();
+console.log(values.next());
+console.log(values.next());
+console.log(values.next());
+console.log(values.next());
 
 // Proxy
 let Jose = {
